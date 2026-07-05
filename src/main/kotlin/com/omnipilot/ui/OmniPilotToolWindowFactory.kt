91667152644cc7d42665315cce39b2c7b5920b75
Tejas Soni <tejashsoni51331@gmail.com>
2026-07-05 22:25:03 +0530

    Feat: Add custom Terminal Bot icon to tool window panel and Marketplace listing

--- src/main/kotlin/com/omnipilot/ui/OmniPilotToolWindowFactory.kt
+++ src/main/kotlin/com/omnipilot/ui/OmniPilotToolWindowFactory.kt
@@ -1,12 +1,17 @@
 package com.omnipilot.ui
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 
 class OmniPilotToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        // Set the tool window icon programmatically for compatibility with all IDE versions
+        val icon = IconLoader.getIcon("/icons/toolWindowIcon.svg", OmniPilotToolWindowFactory::class.java)
+        toolWindow.setIcon(icon)
+
         val chatPanel = OmniPilotChatPanel(project)
         val contentFactory = ContentFactory.getInstance()
         val content = contentFactory.createContent(chatPanel.content, "", false)
